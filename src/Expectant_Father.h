217917ba6f4080231ef413db99e6068808a69b91
Tico0001 <tico0001@gmail.com>
2015-09-01 21:36:27 -0400

    Updates

--- src/Expectant_Father.h
+++ src/Expectant_Father.h
@@ -5,3 +5,8 @@
 #else
 	#define TOP_OFFSET 0
 #endif
+	
+#define KEY_COLOR_RED     0
+#define KEY_COLOR_GREEN   1
+#define KEY_COLOR_BLUE    2
+#define KEY_HIGH_CONTRAST 3
--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -1,43 +1,54 @@
 Pebble.addEventListener('ready', function() 
-{
-	console.log('PebbleKit JS ready!');
-});
+	{
+		console.log('PebbleKit JS ready!');
+	}
+);
 
 Pebble.addEventListener('showConfiguration', function() 
-{
-	var url = 'https://rawgit.com/Tico0001/Expectant-Father/master/config/index.html';
-	console.log('Showing configuration page: ' + url);
-	Pebble.openURL(url);
-});
+	{
+		var url = 'https://rawgit.com/Tico0001/Expectant-Father/master/config/index.html';
+		console.log('Showing configuration page: ' + url);
+		Pebble.openURL(url);
+	}
+);
 
 Pebble.addEventListener('webviewclosed', function(e)
-{
-	var configData = JSON.parse(decodeURIComponent(e.response));
-	console.log('Configuration page returned: ' + JSON.stringify(configData));
-
-	var backgroundColor = configData['background_color'];
-
-	var dict = {};
-	
-	if(configData['high_contrast'] === true) 
 	{
-		dict['KEY_HIGH_CONTRAST'] = configData['high_contrast'];
-	} 
-	else
-	{
-		dict['KEY_COLOR_RED'] = parseInt(backgroundColor.substring(2, 4), 16);
-		dict['KEY_COLOR_GREEN'] = parseInt(backgroundColor.substring(4, 6), 16);
-		dict['KEY_COLOR_BLUE'] = parseInt(backgroundColor.substring(6), 16);
+		var configData = JSON.parse(decodeURIComponent(e.response));
+		console.log('Configuration page returned: ' + JSON.stringify(configData));
+
+// 		var backgroundColor = configData['background_color'];
+
+// 		var dict = {};
+
+// 		if(configData['high_contrast'] === true) 
+// 		{
+// 			dict['KEY_HIGH_CONTRAST'] = configData['high_contrast'];
+// 		} 
+// 		else
+// 		{
+// 			dict['KEY_COLOR_RED'] = parseInt(backgroundColor.substring(2, 4), 16);
+// 			dict['KEY_COLOR_GREEN'] = parseInt(backgroundColor.substring(4, 6), 16);
+// 			dict['KEY_COLOR_BLUE'] = parseInt(backgroundColor.substring(6), 16);
+// 		}
+		
+		  // Prepare AppMessage payload
+		var dict = {
+			'KEY_WIFE_NAME': config_data[wife_name],
+			'KEY_BABY_NAME': config_data[baby_name],
+			'KEY_CONCEP_DATE': config_data[concep_date]
+		};
+
+		// Send to watchapp
+		Pebble.sendAppMessage(dict, 
+			function() 
+			{
+				console.log('Send successful: ' + JSON.stringify(dict));
+			}, 
+			function()
+			{
+				console.log('Send failed!');
+			}
+		);
 	}
-
-	// Send to watchapp
-	Pebble.sendAppMessage(dict, 
-		function() 
-		{
-			console.log('Send successful: ' + JSON.stringify(dict));
-		}, 
-		function()
-		{
-			console.log('Send failed!');
-		});
-});
+);
